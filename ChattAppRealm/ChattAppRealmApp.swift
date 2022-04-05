8c37819c57d416bceaa795158999666fe1b7803b
joanne.yager@student.kyh.se <joanne.yager@student.kyh.se>
2022-04-05 10:36:36 +0200

    Initial Commit

---
+++ ChattAppRealm/ChattAppRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattAppRealmApp.swift
+//  ChattAppRealm
+//
+//  Created by Joanne Yager on 2022-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ChattAppRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
