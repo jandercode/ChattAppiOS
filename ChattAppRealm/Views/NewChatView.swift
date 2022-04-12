76d8944a428491f647ac06b1e19fbbbcaed02407
Luca Salmi <luca.salmi90@gmail.com>
2022-04-12 16:21:40 +0200

    blocks user from creating the same chat(with the same person/people) multiple times

--- ChattAppRealm/Views/NewChatView.swift
+++ ChattAppRealm/Views/NewChatView.swift
@@ -49,7 +49,6 @@ struct NewChatView: View {
                         } label: {
                             Text(label)
                         }
-
                     }
                 }
                 .environment(\.editMode, self.$isEditMode)
@@ -59,26 +58,78 @@ struct NewChatView: View {
                 HStack{
                     
                     Button {
-                        
+                    
                         for user in selection{
                             
                             usersInChat.append(user.username)
-                            
                         }
                         
-                        showChatView = true
+                        removeDoubles()
+                        
+                        if !checkIfChatExists(){
+                            
+                            showChatView = true
+                            
+                        }else{
+                            
+                            print("exists!!")
+                        }
                         
                     } label: {
                         Text("Start Chatting!!")
                     }
-
-                    
                 }
                 NavigationLink(destination: MessagesView(chatId: "", usersInChat: usersInChat), isActive: $showChatView) {
                     EmptyView()
                 }.isDetailLink(false)
                 
             }
+    }
+    
+    
+    func removeDoubles(){
+        
+        var index = -1
+        
+        for item in usersInChat{
+            
+            var counter = 0
+            
+            for item2 in usersInChat{
+                
+                if item == item2{
+                    counter += 1
+                    if counter > 1{
+                        index = usersInChat.firstIndex(of: item2)!
+                    }
+                }
+            }
+            if index > -1{
+                usersInChat.remove(at: index)
+                index = -1
+            }
+        }
+        
+    }
+    
+    
+    func checkIfChatExists() -> Bool{
+        
+        for chat in FirestoreChatDao.firestoreChatDao.chats{
+            
+            if chat.users_in_chat.count == usersInChat.count{
+                
+                if chat.users_in_chat.sorted() == usersInChat.sorted(){
+                    
+                    return true
+                }
+                
+            }
+            
+        }
+        
+        return false
+       
     }
     
     func searchResult() -> [User]{
@@ -98,16 +149,12 @@ struct NewChatView: View {
                     result.append(user)
                     
                 }
-                
             }
-            
         }
         
         return result
         
     }
-    
-    
 }
 
 struct NewChatView_Previews: PreviewProvider {
