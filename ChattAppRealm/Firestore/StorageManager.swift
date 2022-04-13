08eb6a961084a00c4d40d5608d3ac16527862991
Luca Salmi <luca.salmi90@gmail.com>
2022-04-13 14:27:39 +0200

    created storage manager class

---
+++ ChattAppRealm/Firestore/StorageManager.swift
@@ -0,0 +1,101 @@
+//
+//  StorageManager.swift
+//  ChattAppRealm
+//
+//  Created by Luca Salmi on 2022-04-13.
+//
+
+import Foundation
+import Firebase
+import SwiftUI
+
+class StorageManager: ObservableObject{
+    let storage = Storage.storage()
+    
+    
+    func upload(image: UIImage, name: String){
+        
+        let storageRef = storage.reference().child("images/\(name)")
+        
+        
+        // Resize the image to 200px in height with a custom extension
+        let targetSize = CGSize(width: 100, height: 100)
+
+        // Compute the scaling ratio for the width and height separately
+        let widthScaleRatio = targetSize.width / image.size.width
+        let heightScaleRatio = targetSize.height / image.size.height
+
+        // To keep the aspect ratio, scale by the smaller scaling ratio
+        let scaleFactor = min(widthScaleRatio, heightScaleRatio)
+
+        // Multiply the original image’s dimensions by the scale factor
+        // to determine the scaled image size that preserves aspect ratio
+        let _ = CGSize(
+            width: image.size.width * scaleFactor,
+            height: image.size.height * scaleFactor
+        )
+
+        // Convert the image into JPEG and compress the quality to reduce its size
+        let data = image.jpegData(compressionQuality: 0.2)
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        
+        if let data = data {
+                storageRef.putData(data, metadata: metadata) { (metadata, error) in
+                        if let error = error {
+                                print("Error while uploading file: ", error)
+                        }
+
+                        if let metadata = metadata {
+                                print("Metadata: ", metadata)
+                        }
+                }
+        }
+        
+    }
+    
+    
+    func listAllImages(){
+        
+        let storageRef = storage.reference().child("images")
+        
+        storageRef.listAll { (result, error) in
+                if let error = error {
+                        print("Error while listing all files: ", error)
+                }
+
+                for item in result.items {
+                        print("Item in images folder: ", item)
+                }
+        }
+    }
+    
+    func listItem() {
+        // Create a reference
+        let storageRef = storage.reference().child("images")
+
+        // Create a completion handler - aka what the function should do after it listed all the items
+        let handler: (StorageListResult, Error?) -> Void = { (result, error) in
+            if let error = error {
+                print("error", error)
+            }
+
+            let item = result.items
+            print("item: ", item)
+        }
+
+        // List the items
+        storageRef.list(maxResults: 1, completion: handler)
+    }
+
+        // You can use the listItem() function above to get the StorageReference of the item you want to delete
+    func deleteItem(item: StorageReference) {
+        item.delete { error in
+            if let error = error {
+                print("Error deleting item", error)
+            }
+        }
+    }
+    
+}
